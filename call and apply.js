8bceedc68a60869182ddccaa087c4e101c8930b8
Mayank <satimayank74@gmail.com>
2021-04-07 07:23:14 -0700

    currying in js

--- call and apply.js
+++ call and apply.js
@@ -24,3 +24,28 @@ console.log(sum.apply(obj,arr)) // .apply can pass array contains multiple argum
 
 var bound = sum.bind(obj); //it will bind an object with sum and save i bound
 console.log(bound(5,6,7)); // here after creating bind we are passing value in bound
+
+
+// currying in js
+// means copy of function into another function or variable without affecting the original function
+
+// we can curry our function by two ways first is binding and second is closures 
+// curry by bind
+
+let multiple = function(x,y) {
+    console.log('after currying by bind method answer is',x*y);
+}
+
+let mulByBind = multiple.bind(this,2); // now it takes 2 as x inside the variable
+mulByBind(3)// now takes 3 as y if pass another argument then it will take first two only
+
+// curry by closures
+
+let mulClosure = function(x) {
+    return function(y) {
+        console.log('after currying by closures answer is',x*y);
+    }
+}
+
+let mulByClo = mulClosure(2); // takes 2 as x n mulByClo
+mulByClo(3); // 3 as y
